d3d8f2747a524cbcc1fbc02619e89408940448a2
olezhko <alezhko.mail@gmail.com>
2021-02-27 22:54:24 +0300

    ios notify fix
    ios notify fix

--- TrainingDay/TrainingDay.iOS/AppDelegate.cs
+++ TrainingDay/TrainingDay.iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
-﻿using Foundation;
+﻿using System;
+using Foundation;
 using Google.MobileAds;
 using Syncfusion.ListView.XForms.iOS;
 using Syncfusion.XForms.iOS.EffectsView;
@@ -14,7 +15,7 @@ namespace TrainingDay.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -31,15 +32,55 @@ namespace TrainingDay.iOS
             global::Xamarin.Forms.Forms.Init();
             SfListViewRenderer.Init();
             SfEffectsViewRenderer.Init();  //Initialize only when effects view is added to Listview.
-            Firebase.Core.App.Configure();
-            RegisterFirebase();
             LoadApplication(new App(false));
+
+            Firebase.Core.App.Configure();
+
+            Messaging.SharedInstance.Delegate = this;
+            // get permission for notification
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+            }
+            else
+            {
+
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            // Firebase component initialize
+            //Firebase.Analytics.Analytics.App.Configure();
+
+            Firebase.InstanceID.InstanceId.Notifications.ObserveTokenRefresh((sender, e) =>
+            {
+                var token = Messaging.SharedInstance.FcmToken;
+                //var newToken = Firebase.InstanceID.InstanceId.SharedInstance.Token;
+                // if you want to send notification per user, use this token
+                //System.Diagnostics.Debug.WriteLine(newToken);
+
+                //connectFCM();
+            });
+
+
+
             App.ScreenWidth = UIScreen.MainScreen.Bounds.Width;
             App.ScreenHeight = UIScreen.MainScreen.Bounds.Height;
             return base.FinishedLaunching(app, options);
         }
-
-
 
         public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
         {
@@ -66,6 +107,7 @@ namespace TrainingDay.iOS
         [Export("messaging:didReceiveRegistrationToken:")]
         public void DidReceiveRegistrationToken(Messaging messaging, string fcmToken)
         {
+            Firebase.Core.App.Configure();
             // Monitor token generation: To be notified whenever the token is updated.
             //Console.WriteLine("Received token: " + fcmToken);
             // Handle here how your app is storing token locally or send it to server
@@ -79,7 +121,7 @@ namespace TrainingDay.iOS
             {
 
                 // For iOS 10 display notification (sent via APNS)
-                UNUserNotificationCenter.Current.Delegate = new iOSNotificationReceiver();
+                UNUserNotificationCenter.Current.Delegate = this;
 
                 var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
                 UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
@@ -96,5 +138,83 @@ namespace TrainingDay.iOS
 
             UIApplication.SharedApplication.RegisterForRemoteNotifications();
         }
+
+
+        #region Firebase
+
+
+        //public override void DidEnterBackground(UIApplication uiApplication)
+        //{
+        //    Messaging.SharedInstance.Disconnect();
+        //}
+
+        //public override void OnActivated(UIApplication uiApplication)
+        //{
+        //    connectFCM();
+        //    base.OnActivated(uiApplication);
+        //}
+
+//        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+//        {
+//#if DEBUG
+//            Firebase.InstanceID.InstanceId.SharedInstance.SetApnsToken(deviceToken, Firebase.InstanceID.ApnsTokenType.Sandbox);
+//#endif
+//#if RELEASE
+//			Firebase.InstanceID.InstanceId.SharedInstance.SetApnsToken(deviceToken, Firebase.InstanceID.ApnsTokenType.Prod);
+//#endif
+//        }
+
+        // iOS 9 <=, fire when recieve notification foreground
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            Messaging.SharedInstance.AppDidReceiveMessage(userInfo);
+
+            // Generate custom event
+            NSString[] keys = { new NSString("Event_type") };
+            NSObject[] values = { new NSString("Recieve_Notification") };
+            var parameters = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(keys, values, keys.Length);
+
+            // Send custom event
+            Firebase.Analytics.Analytics.LogEvent("CustomEvent", parameters);
+
+            if (application.ApplicationState == UIApplicationState.Active)
+            {
+                System.Diagnostics.Debug.WriteLine(userInfo);
+                var aps_d = userInfo["aps"] as NSDictionary;
+                var alert_d = aps_d["alert"] as NSDictionary;
+                var body = alert_d["body"] as NSString;
+                var title = alert_d["title"] as NSString;
+                debugAlert(title, body);
+            }
+        }
+
+        // iOS 10, fire when recieve notification foreground
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            var title = notification.Request.Content.Title;
+            var body = notification.Request.Content.Body;
+            debugAlert(title, body);
+        }
+
+        //private void connectFCM()
+        //{
+        //    Messaging.SharedInstance.Connect((error) =>
+        //    {
+        //        if (error == null)
+        //        {
+        //            //TODO: Change Topic to what is required
+        //            Messaging.SharedInstance.Subscribe("/topics/all");
+        //        }
+        //        System.Diagnostics.Debug.WriteLine(error != null ? "error occured" : "connect success");
+        //    });
+        //}
+
+        private void debugAlert(string title, string message)
+        {
+            var alert = new UIAlertView(title ?? "Title", message ?? "Message", null, "Cancel", "OK");
+            alert.Show();
+        }
+        #endregion
     }
 }
